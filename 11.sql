5b66b7105c8b6806ac512959903eb23a6a67c7c7
AStoyanoff <chessterfield@gmail.com>
2026-01-21 22:35:00 +0200

    Lab: Subqueries and JOINs

--- 11.sql
+++ 11.sql
@@ -38,7 +38,9 @@ WHERE manager_id IS NULL;
 SELECT
 	COUNT(e.employee_id) AS 'count'
 FROM `employees` AS e
-WHERE e.salary > 
-	(SELECT AVG(salary) AS 'average_salary'
-  FROM `employees`); 
+WHERE e.salary > (
+	SELECT AVG(salary) AS 'average_salary' 
+	FROM `employees`
+);
+   
    
